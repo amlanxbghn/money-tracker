1d543ba2cdb957618ca24e6993bca206f698c04e
amlanxbghn <amlanxconnect@gmail.com>
2024-10-21 22:35:41 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form>
+        <div className='basic'>
+          <input type="text" placeholder={'+200 new samsung tv'} />
+          <input type="datetime-local" />
+        </div>
+        <div className='description'>
+          <input type="text" placeholder={'decription'} />
+        </div>
+        <button type="submit">Add new transaction</button>
+      </form>
+      <div className='transactions'>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>
+              New Samsung TV
+            </div>
+            <div className='description'>
+              Lorem ipsum dolor sit amet
+            </div>
+          </div>
+          <div className='right'>
+            <div className='price red'>$-200</div>
+            <div className='datetime'>2022-01-01 15:45</div>
+          </div>
+        </div>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>
+              New Samsung TV
+            </div>
+            <div className='description'>
+              Lorem ipsum dolor sit amet
+            </div>
+          </div>
+          <div className='right'>
+            <div className='price green'>$+200</div>
+            <div className='datetime'>2022-01-01 15:45</div>
+          </div>
+        </div>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>
+              New Samsung TV
+            </div>
+            <div className='description'>
+              Lorem ipsum dolor sit amet
+            </div>
+          </div>
+          <div className='right'>
+            <div className='price red'>-$900</div>
+            <div className='datetime'>2022-01-01 15:45</div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
